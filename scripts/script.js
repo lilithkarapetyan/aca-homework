92c78e083b9d40bee6497f02eb8581c4e44fd20d
lilithkarapetyan <lilith.karapetyann@gmail.com>
2023-04-15 11:45:27 +0200

    fixing the error

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
+const people = [{
+    name: "Lilith",
+    info: "Some random text",
+    image: "images/profiles/photo.png",
+    isMyTurn: true,
+    link: "pages/page1.html",
+}, {
+    name: "Rafayel",
+    info: "Some random text 1",
+    image: "images/profiles/Rafayel.png",
+    isMyTurn: false,
+    link: "pages/page2.html",
+}, {
+    name: "Arman",
+    info: "Some random text 2",
+    image: "images/profiles/Arman.png",
+    isMyTurn: false,
+    link: "pages/page3.html",
+}];
 
 for (let i = 0; i < people.length; i++) {
     const person = people[i];
